68a03e7676ae0539b485eaa184c757b40acc6859
k-moto <k.motoyama@smartdt.jp>
2017-03-14 04:41:31 +0900

    add detailed01 array

---
+++ ios-detailed-swift-01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-detailed-swift-01
+//
+//  Created by k_motoyama on 2017/03/14.
+//  Copyright © 2017年 k_moto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let arraydemo = arrayDemo()
+        arraydemo.arrayAppend1()
+        arraydemo.arrayAppend2()
+
+    }
+
+
+
+}
+
---
+++ ios-detailed-swift-01/arrayDemo.swift
@@ -0,0 +1,59 @@
+//
+//  array.swift
+//  ios-detailed-swift-01
+//
+//  Created by k_motoyama on 2017/03/14.
+//  Copyright © 2017年 k_moto. All rights reserved.
+//
+
+import Foundation
+
+
+class arrayDemo{
+    
+    init(){
+        
+    }
+    
+    // 空配列の作り方
+    func arrayNewCreate1(){
+        let arr = [String]()
+        
+        print("arr1\(arr.count)")
+    }
+    
+    // 空配列の作り方2
+    func arrayNewCreate2(){
+        let arr: [String] = []
+        
+        print("arr2\(arr.count)")
+    }
+    
+    // 配列内部での計算
+    func arrayCalc(){
+        let data = 1.3
+        let arr = [data * 3.0, data / 10, data + 5.0]
+        
+        print(arr)
+        
+    }
+    
+    // 配列に追加1
+    func arrayAppend1(){
+        
+        var arr = ["a","b","c"]
+        arr.append("d")
+        
+        print("append1\(arr)")
+    }
+    
+    // 配列に追加2
+    func arrayAppend2(){
+        
+        var arr = ["a","b","c"]
+        arr += ["d2"]
+        
+        print("append2\(arr)")
+    }
+    
+}
